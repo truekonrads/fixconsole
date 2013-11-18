2193d19fc7ce6a1c102ac8d10b60b8514064b6ca
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2013-11-18 21:56:50 +0000

    superficially message works

--- lib/fix.rb
+++ lib/fix.rb
@@ -1,97 +1,118 @@
-CHECKSUM=10
-TIMESTAMP=2
-SOH="\1"
+
 module Fix
-	class FixMessage
-		def initialize(opts = {})
-			@fixmsg = {8 => 'FIXT.1.1'}
-			opts.each {|k,v|
-				self.set_tag(k,v)
-			}
-		#	@fixmsg=@fixmsg.merge opts
-		#	self.update_timestamp
-		end
+  CHECKSUM=10
+  TIMESTAMP=2
+  BODYLENGTH=9
+  VERSION=8
+  SOH="\1"
+  class FixMessage
+    def initialize(opts = {})
+      @fixmsg = {8 => 'FIXT.1.1'}
+      opts.each {|k,v|
+        self.set_tag(k,v)
+      }
+    end
 
-		def to_s ()
-			@fixmsg.map {|k,v| "#{k}=#{v}"}.join "|"
-		end
-		def _fix_wo_checksum(msg=nil)
-			if msg.nil?
-				msg=@fixmsg
-			end
+    def to_s ()
+      @fixmsg.map {|k,v| "#{k}=#{v}"}.join "|"
+    end
 
-			s=(((msg.select {|k,v| k!=CHECKSUM}).map {|k,v| "#{k}=#{v}"}).join SOH)+SOH
-		end
-		def to_fix (msg=nil)
-			if msg.nil?
-				msg=@fixmsg
-			end
+    def _fix_wo_checksum(msg=nil)
+      if msg.nil?
+        msg=@fixmsg
+      end
+      #FIXME: Order 8=1st, 9=2nd
+      wo_specials=(msg.select {|k,v| not ([CHECKSUM,VERSION,BODYLENGTH].member? k)})
+      s="8=#{@fixmsg[VERSION]}"+SOH
+      s+="9=#{@fixmsg[BODYLENGTH]}"+SOH
+      s+=((wo_specials.map {|k,v| "#{k}=#{v}"}).join SOH)+SOH
+    end
 
-			s=self._fix_wo_checksum
-			s+"10="+self.checksum(s)+SOH
-		end
+    def to_fix (msg=nil)
+      if msg.nil?
+        msg=@fixmsg
+      end
 
-		def set_tag (tag,value)
-			@fixmsg[tag.to_i]=value
-		end
+      s=self._fix_wo_checksum
+      s+"10="+self.checksum(s)+SOH
+    end
 
-		def self.from_fix(fixstring)
-			opts={}
-			fixstring.split(SOH).each {|pair|
-				k,v=pair.split '='
-				opts[k.to_i]=v
-			}
-			self.new opts
-		end
-		def tags
-			@fixmsg
-		end
+    def set_tag (tag,value)
+      @fixmsg[tag.to_i]=value
+    end
 
-		def checksum (msg)
-			sum=0
-			msg.split("").each {|chr| sum+=chr.ord}
-			"%03d" % (sum % 256)
-		end
-		def update_timestamp(time=nil)
-			if time.nil?
-				time=Time.now
-			end
-			timestring=time.strftime "%Y%m%d-%H:%M:%S.%L"
-			@fixmsg[TIMESTAMP]=timestring
-		end
+    def self.from_fix(fixstring)
+      opts={}
+      fixstring.split(SOH).each {|pair|
+        k,v=pair.split '='
+        opts[k.to_i]=v
+      }
+      self.new opts
+    end
 
-		def update_checksum (sum=nil)
-			if sum.nil?
-				sum=self.checksum
-			end
-			
-			@fixmsg[CHECKSUM]=sum
-			
-		end
+    def tags
+      @fixmsg
+    end
 
-		def prepare_fix
-			self.update_timestamp
-			self.update_length
-			self.update_checksum			
-			self.to_fix
-		end
+    def checksum (msg=nil)
+      if msg.nil?
+    	msg=self._fix_wo_checksum
+      end
+      sum=0
+      msg.split("").each {|chr| sum+=chr.ord}
+      "%03d" % (sum % 256)
+    end
 
-	end
+    def update_timestamp(time=nil)
+      if time.nil?
+        time=Time.now
+      end
+      timestring=time.strftime "%Y%m%d-%H:%M:%S.%L"
+      @fixmsg[TIMESTAMP]=timestring
+    end
 
-	class FixMessageFactory
-		
-		def initialize (opts)
-			@opts=opts
-		end
+    def update_checksum (sum=nil)
+      if sum.nil?
+        sum=self.checksum
+      end
 
-		def make (localopts={})
-			args=@opts.merge localopts
-			FixMessage.new args
-		end
-	end
+      @fixmsg[CHECKSUM]=sum
+      sum
+    end
+    def calc_length(msg=nil)
+    	if msg.nil?
+    		msg=@fixmsg
+    	end
+    	wo_specials=(msg.select {|k,v| not ([VERSION,BODYLENGTH,CHECKSUM].member? k)})
+    	i=0
+    	wo_specials.each {|k,v| i+=k.to_s.length+v.to_s.length+2}
+    	return i
+    end
+    def update_length
+    	self.set_tag(BODYLENGTH,self.calc_length)
+    end
+    def prepare_fix!
+      self.update_timestamp
+      self.update_length
+      self.update_checksum
+    end
 
-	class HeartBeat < FixMessage
+  end
 
-	end
+  class FixMessageFactory
+
+    def initialize (opts)
+      @opts=opts
+    end
+
+    def make (localopts={})
+      args=@opts.merge localopts
+      FixMessage.new args
+    end
+  end
+
+  class HeartBeat < FixMessage
+
+  end
 
 end
--- spec/fix_spec.rb
+++ spec/fix_spec.rb
@@ -2,48 +2,67 @@ RSpec.configure do |config|
   config.mock_framework = :mocha
 end
 require_relative '../lib/fix.rb'
-
+SOH="\1"
 FIXMSG1="8=FIXT.1.1\x019=121\x0135=A\x0149=GSED018437\x0156=TR MATCHING\x0134=3884\x01142=TRFXMAB1234567890\x0152=20131118-14:48:16\x0198=0\x01108=4\x01789=3817\x011137=9\x011407=100\x0110=034\x01"
 
 FIXMSG1_EXPANDED={
-	8 => "FIXT.1.1",
-	9 => "121",
-	35 => "A",
-	49 => "GSED018437",
-	56 => "TR MATCHING",
-	34 => "3884",
-	142 => "TRFXMAB1234567890",
-	52 => "20131118-14:48:16",
-	98 => "0",
-	108 => "4",
-	789 => "3817",
-	1137 => "9",
-	1407 => "100",
-	10 => "034",
+  8 => "FIXT.1.1",
+  9 => "121",
+  35 => "A",
+  49 => "GSED018437",
+  56 => "TR MATCHING",
+  34 => "3884",
+  142 => "TRFXMAB1234567890",
+  52 => "20131118-14:48:16",
+  98 => "0",
+  108 => "4",
+  789 => "3817",
+  1137 => "9",
+  1407 => "100",
+  10 => "034",
 }
 
 describe Fix::FixMessage, "#checksum" do
-	it "calculates a correct checksum" do
-		m=Fix::FixMessage.from_fix(FIXMSG1)
-		m.checksum(m._fix_wo_checksum).should eq("034") 
-	end 
-end 
+  it "calculates a correct checksum" do
+    m=Fix::FixMessage.from_fix(FIXMSG1)
+    m.checksum(m._fix_wo_checksum).should eq("034")
+  end
+end
 
 describe Fix::FixMessage, "#from_fix" do
-	it "parses fix message correctly" do
-		m=Fix::FixMessage.from_fix(FIXMSG1)
-		m.tags.should == FIXMSG1_EXPANDED
-		#puts m
-		#puts FIXMSG1_EXPANDED 
-	end 
-end 
+  it "parses fix message correctly" do
+    m=Fix::FixMessage.from_fix(FIXMSG1)
+    m.tags.should == FIXMSG1_EXPANDED
+    #puts m
+    #puts FIXMSG1_EXPANDED
+  end
+end
+describe Fix::FixMessage, "#calc_length" do
+  it "parses fix message correctly" do
+    m=Fix::FixMessage.from_fix(FIXMSG1)
+    m.calc_length.to_s.should  eq("121")
 
+    #puts m
+    #puts FIXMSG1_EXPANDED
+  end
+end
 describe Fix::FixMessage, "#to_fix" do
-	it "serialises fix message correctly" do
-		orig=Fix::FixMessage.from_fix(FIXMSG1)
-		hobbit=Fix::FixMessage.from_fix(orig.to_fix)
-		orig.tags.should == hobbit.tags
-		#puts m
-		#puts FIXMSG1_EXPANDED 
-	end 
-end 
+  it "serialises fix message correctly" do
+    orig=Fix::FixMessage.from_fix(FIXMSG1)
+    hobbit=Fix::FixMessage.from_fix(orig.to_fix)
+    orig.tags.should == hobbit.tags
+    #puts m
+    #puts FIXMSG1_EXPANDED
+  end
+  it "puts tags in correct order" do
+    m=Fix::FixMessage.from_fix(FIXMSG1)
+    m.prepare_fix!
+    pairs=(m.to_fix).split SOH
+    # Tag 8 comes 1st
+    tag,value=pairs[0].split "="
+    tag.should eq("8")
+    # Tag 9 comes 2nd
+    tag,value=pairs[1].split "="
+    tag.should eq("9") 
+  end
+end
